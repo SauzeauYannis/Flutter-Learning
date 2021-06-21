f061a86ed3567da058a8a28bb63afc2c1679387c
SauzeauYannis <sauzeauyannis@gmail.com>
2021-06-21 12:36:09 +0200

    Add a windows app that uses the GitHub GraphQL API

---
+++ github_graphql_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,4 @@
+const githubClientId = '92fe13953e717caf5265';
+const githubClientSecret = '77f94a3569e66df2478323299a8d1b12d8b9f484';
+
+const githubScopes = ['repo', 'read:org'];
---
+++ github_graphql_client/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:github_graphql_client/github_oauth_credentials.dart';
+import 'package:github_graphql_client/src/github_gql/github_queries.data.gql.dart';
+import 'package:github_graphql_client/src/github_gql/github_queries.req.gql.dart';
+import 'package:github_graphql_client/src/github_login.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:gql_link/gql_link.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitHub GraphQL API Client',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'GitHub GraphQL API Client'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(
+      builder: (context, httpClient) {
+        final link = HttpLink(
+            'https://api.github.com/graphql', httpClient: httpClient);
+        return FutureBuilder<GViewerDetailData_viewer>(
+          future: viewerDetail(link),
+          builder: (_, snapshot) =>
+              Scaffold(
+                appBar: AppBar(
+                  title: Text(title),
+                ),
+                body: Center(
+                  child: Text(
+                      snapshot.hasData ? 'Hello ${snapshot.data!.login}' : 'Retrieving viewer login details...'),
+                ),
+              ),
+        );
+      },
+      githubClientId: githubClientId,
+      githubClientSecret: githubClientSecret,
+      githubScopes: githubScopes,
+    );
+  }
+}
+
+Future<GViewerDetailData_viewer> viewerDetail(Link link) async {
+  final req = GViewerDetail((b) => b);
+  final result = await link
+      .request(Request(operation: req.operation, variables: req.vars.toJson()))
+      .first;
+  final errors = result.errors;
+  if (errors != null && errors.isNotEmpty)
+    throw QueryException(errors);
+  return GViewerDetailData.fromJson(result.data!)!.viewer;
+}
+
+class QueryException implements Exception {
+  QueryException(this.errors);
+
+  List<GraphQLError> errors;
+
+  @override
+  String toString() {
+    return 'Query Exception: ${errors.map((err) => '$err').join(',')}';
+  }
+}
